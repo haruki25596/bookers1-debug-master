a23794d97aab475fb30dea061312152cbb5779ef
haruki25596 <gymnasticc.1950.softbank.jp@gmail.com>
2021-05-23 02:33:55 +0000

    フィニッシュ5

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,28 +1,28 @@
 class BooksController < ApplicationController
-  def top
-  end
-
-  def index
-    @books = Book.all
-    @book = Book.new
-  end
-
-  def create
-    
-    @book = Book.new(book_params)
-    if @book.save
-      flash[:notice] = "Book was successfully created."
-      redirect_to book_path(@book.id)
-    else
-      @books = Book.all
-      render :index
-      
+    def top
     end
-  end
 
-  def show
-    @book = Book.find(params[:id])
-  end
+   def index
+     @books = Book.all
+      @book = Book.new
+   end
+
+    def create
+    
+     @book = Book.new(book_params)
+     if @book.save
+        flash[:notice] = "Book was successfully created."
+        redirect_to book_path(@book.id)
+     else
+       @books = Book.all
+       render :index
+      
+     end
+    end
+
+    def show
+     @book = Book.find(params[:id])
+    end
 
   def edit
     @book = Book.find(params[:id])
